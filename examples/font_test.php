da2eac730e01bf72706c1650a538834a58bd76ad
wjinbo <wangjb@pvc123.com>
2022-07-07 09:29:58 +0800

    feat(main): 增加台北黑体字体库，修复部分字体不显示问题(滘);

---
+++ examples/font_test.php
@@ -0,0 +1,24 @@
+<?php
+require_once(__DIR__ . '/../vendor/autoload.php');
+
+$pdf = new \Pdf\Pdf();
+
+// remove default header/footer
+$pdf->setPrintHeader(false);
+$pdf->setPrintFooter(false);
+
+$pdf->setMbStr(true);
+$pdf->setHeaderMargin(10);
+$pdf->AddPage();
+$pdf->AddFont('tbht_r', '');
+
+$pdf->setFont('tbht_r', '', 10.5);
+
+$pdf->p('英文 qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM');
+$pdf->p('数字 0123456789');
+$pdf->p('符号 `~!@#$%^&*()-=_+[]\{}|;\':",./<>?/*-+');
+$pdf->p('符号 ·~！@#￥%……&*（）-=——+【】、「」|；‘：“，。、《》？/*-+');
+$pdf->p('方正黑体显示失败   道滘(jiao)    鹓鶵(yuan\'chu)');
+$pdf->p('魑魅魍魉(chi\'mei\'wang\'liang)');
+
+$pdf->Output(__DIR__ . '/font_test.pdf', 'F');
